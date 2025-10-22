c963c4b1a355f734a9310d72e4e34dc09824b7bb
Александра <sdarganool@gmail.com>
2025-10-22 16:33:02 +0300

    Add Models and Controllers

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\SaleController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'API is working']);
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->group( function () {
+    Route::get('/sales', [SaleController::class, 'index']);
+    Route::get('/orders', [\App\Http\Controllers\OrderController::class, 'index']);
+    Route::get('/stocks', [\App\Http\Controllers\StockController::class, 'index']);
+    Route::get('/incomes', [\App\Http\Controllers\IncomeController::class, 'index']);
+
+});
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\SaleController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
